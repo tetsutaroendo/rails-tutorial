8a14005d8dc68891d60b3bde5905eb33d96b7661
tetsutaroendo <tetsutaro.endo@gmail.com>
2017-03-02 01:58:03 +0900

    Add view helper of the title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return base_title if page_title.empty?
+    page_title + ' | ' + base_title
+  end
 end
