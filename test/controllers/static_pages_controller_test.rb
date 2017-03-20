16661321953564c436f5ce3538f41429d46a2895
tetsutaroendo <tetsutaro.endo@gmail.com>
2017-03-20 16:41:51 +0900

    Change all page pathes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,19 +6,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get root" do
+  test "should get root(home)" do
     get root_url
-    assert_response :success
-  end
-
-  test "should get home" do
-    get static_pages_home_url
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
@@ -30,7 +25,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
