58db4c4da7b17b542003e369f852fba261426a5e
tetsutaroendo <tetsutaro.endo@gmail.com>
2017-04-17 23:43:14 +0900

    Add flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # This represents user_url(@user)
     else
       render :new
     end
