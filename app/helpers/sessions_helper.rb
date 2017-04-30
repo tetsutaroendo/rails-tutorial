292d254e7de42c89d9d330f7e84806517aa359cc
tetsutaroendo <tetsutaro.endo@gmail.com>
2017-05-01 02:59:19 +0900

    Add forget cookies func

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def current_user
     if(user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -26,6 +32,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
