cb8ebec5a1bfe79d57e42d894636177a05fb1df1
tetsutaroendo <tetsutaro.endo@gmail.com>
2017-05-27 22:54:28 +0900

    Fix hash literal

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
-      region: ENV['S3_REGION'],
-      aws_access_key_id: ENV['S3_ACCESSKEY'],
-      aws_secret_access_key_id: ENV['S3_SECRET_KEY']
+      :provider => 'AWS',
+      :region => ENV['S3_REGION'],
+      :aws_access_key_id => ENV['S3_ACCESSKEY'],
+      :aws_secret_access_key_id => ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
